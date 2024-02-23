7a787bf6b3d3f88b0f78fa00d5a3ba7aa6ef6a88
moj813 <mojonly813@gmail.com>
2024-02-23 13:36:49 +0530

    Merge

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from "react";
-import {Route,Routes} from 'react-router-dom'
+import React, { useState } from "react";
+import {Navigate, Route,Routes} from 'react-router-dom'
 import Admin from './layouts/admin/index'
 import User from './layouts/user/index'
 import Mytournaments from "./layouts/admin/views/mytournaments/Mytournaments";
 function App() {
+  const [isUser , setUser] = useState(true);
+  const [isAdmin , setAdmin] = useState(true);
   return (
     <Routes>
         <Route path="/" element={<>This is Home Page</>}/>
@@ -11,10 +13,16 @@ function App() {
     {/* This is For Admin ONly  */}
 
         <Route path='/admin' element={<Admin />}>
-          <Route path="/admin/default" element={<>This is Admin Degfault</> } />
-          <Route path="/admin/mytournaments" element={<Mytournaments/> } />
-          <Route path="/admin/*" element={<>404 PAge Not Found</>} />
+              <Route>
+                <Route path="/admin/default" element={<>This is Admin Degfault</> } />
+                <Route path="/admin/mytournaments" element={<Mytournaments/> } />
+                <Route path="/admin/*" element={<>404 PAge Not Found</>} />
+              </Route>
         </Route>
+
+
+
+    
 
 
         {/* This is for User only  */}
@@ -25,6 +33,7 @@ function App() {
           <Route path="/user/*" element={<>404 PAge Not Found</>} />
         </Route>
 
+        <Route path="/login" element={<>This is Login</>} />
         <Route path="*" element={<>404 PAge Not Found</>} />
 
     </Routes>
