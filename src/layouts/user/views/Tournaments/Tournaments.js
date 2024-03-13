babdcfa7dfdf3b18744564b1621cf1d76b7852be
Darshil Marakana <marakanadarshil@gmail.com>
2024-03-13 23:34:40 +0530

    user

---
+++ src/layouts/user/views/Tournaments/Tournaments.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Tournaments.css';
+
+const Tournaments = ({ tournaments }) => {
+  return (
+    <div className="main-component">
+      <h2>Tournaments</h2>
+      <div className="tournament-list">
+        {tournaments && tournaments.map((tournament, index) => (
+          <div className="cards" key={index}>
+            <div className="card">
+              <span>From {new Date(tournament.startDate).toLocaleDateString()} to {new Date(tournament.lastDate).toLocaleDateString()}</span>
+              <h2>{tournament.tournamentName}</h2>
+              <p>{tournament.cityName}</p>
+              <h6>Tap to enroll</h6>
+            </div>
+          </div>
+        ))}
+        {!tournaments && <p>No tournaments available</p>}
+      </div>
+    </div>
+  );
+}
+
+export default Tournaments;
